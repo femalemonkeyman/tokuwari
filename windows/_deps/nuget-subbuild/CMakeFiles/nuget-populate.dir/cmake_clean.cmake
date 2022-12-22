47bd525cfbe73280cd0fb19ba95486fc7cb36ae8
Joey <joeymacdonald55@gmail.com>
2022-12-22 12:01:55 -0500

    upgrade dependencies

---
+++ windows/_deps/nuget-subbuild/CMakeFiles/nuget-populate.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/mnt/Storage/HomeStuff/Documents/anicross/windows/_deps/nuget-src/nuget.exe"
+  "CMakeFiles/nuget-populate"
+  "CMakeFiles/nuget-populate-complete"
+  "nuget-populate-prefix/src/nuget-populate-stamp/nuget-populate-build"
+  "nuget-populate-prefix/src/nuget-populate-stamp/nuget-populate-configure"
+  "nuget-populate-prefix/src/nuget-populate-stamp/nuget-populate-copyfile"
+  "nuget-populate-prefix/src/nuget-populate-stamp/nuget-populate-download"
+  "nuget-populate-prefix/src/nuget-populate-stamp/nuget-populate-install"
+  "nuget-populate-prefix/src/nuget-populate-stamp/nuget-populate-mkdir"
+  "nuget-populate-prefix/src/nuget-populate-stamp/nuget-populate-patch"
+  "nuget-populate-prefix/src/nuget-populate-stamp/nuget-populate-test"
+  "nuget-populate-prefix/src/nuget-populate-stamp/nuget-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/nuget-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,7 +6,7 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
+#include <connectivity_plus/connectivity_plus_windows_plugin.h>
 #include <dart_vlc/dart_vlc_plugin.h>
 #include <flutter_native_view/flutter_native_view_plugin.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
