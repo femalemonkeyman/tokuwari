d8919f8ca620b439e6d1c83a03ad92d029e222fd
femalemonkeyman <joeymacdonald55@gmail.com>
2023-10-22 12:36:50 -0400

    Many many changes, don't build yet

---
+++ lib/media/media_controllers.dart
@@ -0,0 +1,43 @@
+import 'package:async/async.dart';
+import 'package:preload_page_view/preload_page_view.dart';
+import 'package:tokuwari_models/info_models.dart';
+
+enum ReaderDirection {
+  vertical,
+  horizontal,
+
+  ///Please place manga in front of mirror and re-evaluate yourself
+  wrong,
+}
+
+class ReaderController {
+  final List<MediaProv> chapters;
+  final PreloadPageController controller;
+  late CancelableOperation load = CancelableOperation.fromFuture(fetch());
+  Chapter chapter = const Chapter(pages: []);
+  ReaderDirection direction = ReaderDirection.wrong;
+  int current;
+  bool loading = false;
+
+  ReaderController({required this.chapters, required this.controller, this.current = 0});
+
+  bool get hasNext => current != chapters.length - 1;
+  bool get hasPrevious => current > 0;
+  bool get reverse => (direction == ReaderDirection.wrong) ? false : true;
+
+  Future<void> fetch() async {
+    chapter = await chapters[current].call!();
+  }
+
+  Future<void> changeChapter(final bool forward) async {
+    loading = true;
+    (forward) ? current++ : current--;
+    load = CancelableOperation.fromFuture(fetch());
+    await load.value.then((value) => controller.jumpToPage((forward) ? 0 : chapter.pages.length - 1));
+    loading = false;
+  }
+
+  void dispose() {
+    load.cancel();
+  }
+}
